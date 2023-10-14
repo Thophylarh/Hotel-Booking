15627abce642cbea83a2165fe5d9a741c8222276
Thophylarh <iwualathophylarh@gmail.com>
2023-10-14 11:55:24 +0100

    navbar component

--- src/components/block/sidebar/index.tsx
+++ src/components/block/sidebar/index.tsx
@@ -1,63 +1,92 @@
 import Image from "next/image";
 import React from "react";
-import {AiFillHome } from 'react-icons/ai';
-import {TbGridDots} from 'react-icons/tb';
-import {BsFillTicketDetailedFill} from 'react-icons/bs';
-import {AiOutlineHeart} from 'react-icons/ai';
-import {AiOutlineSetting} from 'react-icons/ai';
-import AvatarImg from "../../../assets/jpg/avatar.jpg"
+import { AiFillHome } from "react-icons/ai";
+import { TbGridDots } from "react-icons/tb";
+import { BsFillTicketDetailedFill } from "react-icons/bs";
+import { AiOutlineHeart } from "react-icons/ai";
+import { AiOutlineSetting } from "react-icons/ai";
+import { MdAddCircle } from "react-icons/md";
+import { MdOutlineLogout } from "react-icons/md";
+import AvatarImg from "../../../assets/jpg/avatar.jpg";
 
 type Props = {};
 
 const sidebarItems = [
   {
     id: 0,
-    icon: <AiFillHome/>,
+    icon: <AiFillHome />,
     title: "Dashboard",
   },
   {
     id: 1,
-    icon: <TbGridDots/>,
+    icon: <TbGridDots />,
     title: "Explore City",
   },
   {
     id: 2,
-    icon: <BsFillTicketDetailedFill/>,
+    icon: <BsFillTicketDetailedFill />,
     title: "Ticket",
   },
   {
     id: 3,
-    icon: <AiOutlineHeart/>,
+    icon: <AiOutlineHeart />,
     title: "Favorites",
   },
   {
     id: 4,
-    icon: <AiOutlineSetting/>,
+    icon: <AiOutlineSetting />,
     title: "Settings",
   },
 ];
 
 function Sidebar({}: Props) {
   return (
-    <main>
-      <div className="logo">
-        <div>
-          <h2>Mima Booking</h2>
-          <div>
-            <Image src={AvatarImg} alt="Avatar" className="w-12 h-12 rounded-full" />
+    <main className="max-h-screen lg:block overflow-y-scroll bg-[#fff] min-w-[250px] py-4">
+      <div className="logo  flex flex-col items-center">
+        <div className="">
+          <div className="mx-auto flex flex-col items-center space-y-4">
+            <h2 className="text-[#1a1b22] text-2xl font-bold">
+              Mima <span className="text-[#636363]">Booking</span>{" "}
+            </h2>
+            <div className="relative">
+              <div className="border-2 w-20 h-20 rounded-full flex items-center justify-center">
+                <Image
+                  src={AvatarImg}
+                  alt="Avatar"
+                  className="w-16 h-16 rounded-full"
+                />
+              </div>
+              <div className="absolute -bottom-3 left-8">
+                <MdAddCircle size={20} />
+              </div>
+            </div>
+
+            <p className="text-[#636363] text-base font-medium">illa jan</p>
           </div>
-          <p>illa jan</p>
         </div>
-        <div className="nav-content">
-          <div className="flex items-center gap-x-4">
+        <div className="nav-content ">
+          <div className="py-20">
+          <div className="space-y-5 ">
             {sidebarItems.map(({ icon, title, id }) => (
-              <div key={id}>
-                {icon}
-                <p>{title}</p>
+              <div key={id} className="flex items-center gap-x-4">
+                <div className="text-[#919191]">{icon}</div>
+
+                <p className="text-[#919191]">{title}</p>
               </div>
             ))}
           </div>
+        
+          </div>
+          <div className="logout flex items-center gap-x-4 last:mb-0">
+          <div className="text-[#919191]">
+            <MdOutlineLogout />
+          </div>
+
+          <p className="text-[#919191]">Logout</p>
         </div>
+        
+        </div>
+       
       </div>
     </main>
   );
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,14 +1,23 @@
-import Sidebar from '@/components/block/sidebar'
-import React from 'react'
+import Navbar from "@/components/block/nav";
+import Sidebar from "@/components/block/sidebar";
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 function Dashboard({}: Props) {
   return (
-    <div>Dashboard
-        <Sidebar/>
+    <div className="">
+      <div className="flex">
+        <Sidebar />
+        <div className="bg-[#f9f9f9] w-full h-screen">
+            <div>
+            <Navbar />
+            </div>
+        
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
